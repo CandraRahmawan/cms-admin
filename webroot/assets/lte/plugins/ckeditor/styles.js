0e57f5d295986813a7e50056ea215205deed770b
Candra Rahmawan <candraa.rahmawan@gmail.com>
2020-04-05 13:37:59 +0700

    CR: Remove div wrapper specification on ckeditor

--- webroot/assets/lte/plugins/ckeditor/styles.js
+++ webroot/assets/lte/plugins/ckeditor/styles.js
@@ -59,8 +59,6 @@ CKEDITOR.stylesSet.add( 'default', [
 
 	{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
 
-	{ name: 'Specification List',element: 'div', attributes: { 'class': 'specification-list' } },
-	{ name: 'Specification',	element: 'div', attributes: { 'class': 'specification' } },
 	{ name: 'Span',				element: 'span'},
 
 	{ name: 'Big',				element: 'big' },
