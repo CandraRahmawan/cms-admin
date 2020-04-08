52e6f03d445cb355c32c0494112730720a988760
Candra Rahmawan <candraa.rahmawan@gmail.com>
2020-04-08 11:33:46 +0700

    CR: Update dump sql

--- sql/cms.sql
+++ sql/cms.sql
@@ -27,7 +27,7 @@ CREATE TABLE `category` (
   `name` varchar(50) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `status` enum('Y','N','T') NOT NULL COMMENT 'Y : active, N : non active, T : trash',
-  `type` enum('Gallery','Content','Page','Slider Banner','Section') NOT NULL,
+  `type` enum('Gallery','Content','Page','Slider Banner','Section','Product') NOT NULL,
   `create_date` datetime NOT NULL,
   `update_date` datetime DEFAULT NULL,
   `trash_date` datetime DEFAULT NULL,
@@ -228,7 +228,7 @@ CREATE TABLE `plugins` (
   UNIQUE KEY `key` (`key`),
   KEY `id_theme` (`id_theme`),
   CONSTRAINT `FK_plugins_themes` FOREIGN KEY (`id_theme`) REFERENCES `themes` (`id_theme`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -237,7 +237,7 @@ CREATE TABLE `plugins` (
 
 LOCK TABLES `plugins` WRITE;
 /*!40000 ALTER TABLE `plugins` DISABLE KEYS */;
-INSERT INTO `plugins` VALUES (1,'page','2019-02-21 22:45:45',NULL,'Service Page',NULL,'service_page','service_page','Y',1),(2,'section','2019-04-08 22:39:16',NULL,'Area Coverage',NULL,'area_coverage','area_coverage','Y',1),(3,'section','2019-04-17 09:01:34',NULL,'Why Choose Us',NULL,'why_choose_us','why_choose_us','Y',1),(4,'page','2020-02-17 16:17:18',NULL,'Product Category Page',NULL,'product_category_page','product_category_page','Y',2),(5,'page','2020-02-20 06:13:32',NULL,'Where To Buy Page',NULL,'where_to_buy_page','template_multi_input2','Y',2),(6,'page','2020-02-20 14:27:01',NULL,'Our Story Page',NULL,'our_story_page','dbemotion_our_story_page','Y',2),(7,'video','2020-03-15 17:31:59',NULL,'Youtube Review',NULL,'youtube_review','youtube_review','Y',2),(8,'section','2020-03-29 15:34:23',NULL,'Homepage Section 1',NULL,'section_1','template_single_input3','Y',2),(9,'section','2020-03-29 16:04:30',NULL,'Homepage Section 2',NULL,'section_2','template_single_input2','Y',2),(10,'section','2020-03-29 16:10:38',NULL,'Homepage Section 3',NULL,'section_3','template_single_input4','Y',2),(11,'section','2020-03-29 17:35:00',NULL,'Homepage Section 4',NULL,'section_4','template_multi_input2','Y',2),(12,'section','2020-03-30 08:53:44',NULL,'Service Centre List',NULL,'service_centre','template_multi_input2_no_image','Y',2),(13,'page','2020-03-30 09:50:24',NULL,'Contact Page',NULL,'contact_page','template_single_input2_no_image','Y',2),(14,'page','2020-03-30 09:58:56',NULL,'Warranty Information Page',NULL,'warranty_information_page','template_single_input3_no_image','Y',2);
+INSERT INTO `plugins` VALUES (1,'page','2019-02-21 22:45:45',NULL,'Service Page',NULL,'service_page','service_page','Y',1),(2,'section','2019-04-08 22:39:16',NULL,'Area Coverage',NULL,'area_coverage','area_coverage','Y',1),(3,'section','2019-04-17 09:01:34',NULL,'Why Choose Us',NULL,'why_choose_us','why_choose_us','Y',1),(4,'page','2020-02-17 16:17:18',NULL,'Product Category Page',NULL,'product_category_page','product_category_page','Y',2),(5,'page','2020-02-20 06:13:32',NULL,'Where To Buy Page',NULL,'where_to_buy_page','template_multi_input2','Y',2),(6,'page','2020-02-20 14:27:01',NULL,'Our Story Page',NULL,'our_story_page','dbemotion_our_story_page','Y',2),(7,'video','2020-03-15 17:31:59',NULL,'Youtube Review',NULL,'youtube_review','youtube_review','Y',2),(8,'section','2020-03-29 15:34:23',NULL,'Homepage Section 1',NULL,'section_1','template_single_input3','Y',2),(9,'section','2020-03-29 16:04:30',NULL,'Homepage Section 2',NULL,'section_2','template_single_input2','Y',2),(10,'section','2020-03-29 16:10:38',NULL,'Homepage Section 3',NULL,'section_3','template_single_input4','Y',2),(11,'section','2020-03-29 17:35:00',NULL,'Homepage Section 4',NULL,'section_4','template_multi_input2','Y',2),(12,'section','2020-03-30 08:53:44',NULL,'Service Centre List',NULL,'service_centre','template_multi_input2_no_image','Y',2),(13,'page','2020-03-30 09:50:24',NULL,'Contact Page',NULL,'contact_page','template_single_input2_no_image','Y',2),(14,'page','2020-03-30 09:58:56',NULL,'Warranty Information Page',NULL,'warranty_information_page','template_single_input3_no_image','Y',2),(15,'section','2020-04-02 22:59:57',NULL,'Featured Category Product',NULL,'featured_category_product','featured_category_product','Y',2);
 /*!40000 ALTER TABLE `plugins` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -260,6 +260,48 @@ CREATE TABLE `plugins_detail` (
   KEY `FK_plugin_detail_plugin` (`plugin_id`),
   CONSTRAINT `FK_plugin_detail_plugin` FOREIGN KEY (`plugin_id`) REFERENCES `plugins` (`plugin_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `product_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `unique_id` char(15) NOT NULL,
+  `subtitle` varchar(200) DEFAULT NULL,
+  `description_1` text DEFAULT NULL,
+  `description_2` text DEFAULT NULL,
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_date` datetime DEFAULT NULL,
+  `price` varchar(70) NOT NULL,
+  `prefix_currency` char(8) NOT NULL,
+  `specification` mediumtext DEFAULT NULL,
+  `features` text DEFAULT NULL,
+  `features_color` varchar(10) NOT NULL DEFAULT '#fff',
+  `feature_note` varchar(500) DEFAULT NULL,
+  `img_path` text DEFAULT NULL,
+  `additional_info` text DEFAULT NULL,
+  `status` enum('Y','N') NOT NULL DEFAULT 'N',
+  `render_template_filename` varchar(100) NOT NULL,
+  `category_id` smallint(6) NOT NULL,
+  `author` smallint(6) NOT NULL,
+  `last_updated_by` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  UNIQUE KEY `products_UN` (`unique_id`),
+  KEY `products_FK` (`category_id`),
+  KEY `products_FK_1` (`author`),
+  KEY `products_FK_2` (`last_updated_by`),
+  CONSTRAINT `products_FK` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`),
+  CONSTRAINT `products_FK_1` FOREIGN KEY (`author`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `products_FK_2` FOREIGN KEY (`last_updated_by`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 -- --------------------------------------------------------
@@ -346,11 +388,11 @@ CREATE TABLE `themes_setting` (
   `id_theme` tinyint(4) NOT NULL,
   `key` varchar(100) NOT NULL,
   `field_name` varchar(100) NOT NULL,
-  `group` enum('footer','social_media','section','company','contact','image','heading','plugin') DEFAULT NULL,
-  `value_1` mediumtext DEFAULT NULL,
+  `group` enum('footer','template','social_media','section','company','contact','image','heading','plugin') DEFAULT NULL,
+  `value_1` text DEFAULT NULL,
   `is_active` enum('Y','N') NOT NULL DEFAULT 'Y',
   `type` enum('embed','text','image') NOT NULL DEFAULT 'text',
-  `category` enum('Slider Banner','Page','Section','Plugin') DEFAULT NULL,
+  `category` enum('Slider Banner','Page','Section','Plugin','MultiSelect') DEFAULT NULL,
   PRIMARY KEY (`id_theme`,`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -361,7 +403,7 @@ CREATE TABLE `themes_setting` (
 
 LOCK TABLES `themes_setting` WRITE;
 /*!40000 ALTER TABLE `themes_setting` DISABLE KEYS */;
-INSERT INTO `themes_setting` VALUES (1,'area_coverage','Area Coverage Plugin','plugin','','Y','embed','Plugin'),(1,'area_coverage_title','Area Coverage Title','plugin','','Y','text',NULL),(1,'company_title','Company Title','company','','Y','text',NULL),(1,'contact_email','Contact Email','contact','','Y','text',NULL),(1,'contact_phone','Contact Phone','contact','','Y','text',NULL),(1,'contact_whatsapp','Contact Whatsapp','contact','','Y','text',NULL),(1,'footer_address','Footer Address','footer','','Y','text',NULL),(1,'footer_address_title','Footer Address Title','heading','','Y','text',NULL),(1,'footer_copyright','Footer CopyRight','footer','','Y','text',NULL),(1,'footer_title_blog','Footer Artikel Title','heading','','Y','text',NULL),(1,'footer_title_contact','Footer Contact Title','heading','','Y','text',NULL),(1,'image_logo','Image Logo','image','','Y','image',NULL),(1,'menu_header','Menu Header',NULL,'1','Y','embed','Page'),(1,'path_url_admin','Path Url Admin',NULL,'','Y','text',NULL),(1,'section_1','Section Welcome Homepage','section','','Y','embed','Section'),(1,'section_1_title','Title Welcome Homepage','heading','','Y','text',NULL),(1,'service_page','Service Page Plugin','plugin','','Y','embed','Plugin'),(1,'service_page_title','Service Page Title','plugin','','Y','text',NULL),(1,'top_image_carousel','Homepage Image Banner','image','','Y','embed','Slider Banner'),(1,'why_choose_us','Why Choose Us Plugin','plugin','','Y','embed','Plugin'),(1,'why_choose_us_title','Why Choose Us Title','plugin','','Y','text',NULL),(2,'captcha_secret_key','Captcha Secret Key',NULL,'6LcrnOEUAAAAANmHnOZ9ghsa_b0844FzRd1pDKac','Y','text',NULL),(2,'captcha_site_key','Captcha Site Key',NULL,'6LcrnOEUAAAAAMqhYt3AY5lXkkNDEWcGfyM8DvP9','Y','text',NULL),(2,'contact_page','Contact Page','plugin','13','Y','embed','Plugin'),(2,'email','Email','contact','info@dbe-id.com','Y','text',NULL),(2,'facebook','Facebook Link','social_media','https://www.facebook.com/DbE-Indonesia-111625933761626/','Y','text',NULL),(2,'featured_top_image_carousel','Featured Homepage Top Image Carousel',NULL,'2','Y','embed','Slider Banner'),(2,'instagram','Instagram Link','social_media','https://www.instagram.com/dbe.id/','Y','text',NULL),(2,'menu_footer','Menu Footer',NULL,'1','Y','embed','Page'),(2,'menu_header','Menu Header',NULL,'1','Y','embed','Page'),(2,'our_story_page','Our Story Page','plugin','6','Y','embed','Plugin'),(2,'phone_number','Phone Number','contact','+62 22 423 4642','Y','text',NULL),(2,'product_category_page','Product Category Page Plugin','plugin','4','Y','embed','Plugin'),(2,'section_1','Homepage Section 1','plugin','8','Y','embed','Plugin'),(2,'section_2','Homepage Section 2','plugin','9','Y','embed','Plugin'),(2,'section_3','Homepage Section 3','plugin','10','Y','embed','Plugin'),(2,'section_4','Homepage Section 4','plugin','11','Y','embed','Plugin'),(2,'service_centre','Service Centre List','plugin','12','Y','embed','Plugin'),(2,'shopee','Shopee Link','social_media','https://shopee.co.id/dbeofficial','Y','text',NULL),(2,'tokopedia','Tokopedia Link','social_media','https://www.tokopedia.com/dbeofficial','Y','text',NULL),(2,'top_image_carousel','Homepage Top Image Carousel',NULL,'1','Y','embed','Slider Banner'),(2,'warranty_information_page','Warranty Information Page','plugin','14','Y','embed','Plugin'),(2,'whatsapp_number','WhatsApp Number','contact','6281319991783','Y','text',NULL),(2,'where_to_buy_page','Where to Buy Page','plugin','5','Y','embed','Plugin'),(2,'youtube_review','Youtube Review Plugin','plugin','7','Y','embed','Plugin');
+INSERT INTO `themes_setting` VALUES (1,'area_coverage','Area Coverage Plugin','plugin','','Y','embed','Plugin'),(1,'area_coverage_title','Area Coverage Title','plugin','','Y','text',NULL),(1,'company_title','Company Title','company','','Y','text',NULL),(1,'contact_email','Contact Email','contact','','Y','text',NULL),(1,'contact_phone','Contact Phone','contact','','Y','text',NULL),(1,'contact_whatsapp','Contact Whatsapp','contact','','Y','text',NULL),(1,'footer_address','Footer Address','footer','','Y','text',NULL),(1,'footer_address_title','Footer Address Title','heading','','Y','text',NULL),(1,'footer_copyright','Footer CopyRight','footer','','Y','text',NULL),(1,'footer_title_blog','Footer Artikel Title','heading','','Y','text',NULL),(1,'footer_title_contact','Footer Contact Title','heading','','Y','text',NULL),(1,'image_logo','Image Logo','image','','Y','image',NULL),(1,'menu_header','Menu Header',NULL,'1','Y','embed','Page'),(1,'path_url_admin','Path Url Admin',NULL,'','Y','text',NULL),(1,'section_1','Section Welcome Homepage','section','','Y','embed','Section'),(1,'section_1_title','Title Welcome Homepage','heading','','Y','text',NULL),(1,'service_page','Service Page Plugin','plugin','','Y','embed','Plugin'),(1,'service_page_title','Service Page Title','plugin','','Y','text',NULL),(1,'top_image_carousel','Homepage Image Banner','image','','Y','embed','Slider Banner'),(1,'why_choose_us','Why Choose Us Plugin','plugin','','Y','embed','Plugin'),(1,'why_choose_us_title','Why Choose Us Title','plugin','','Y','text',NULL),(2,'captcha_secret_key','Captcha Secret Key',NULL,'6LcrnOEUAAAAANmHnOZ9ghsa_b0844FzRd1pDKac','Y','text',NULL),(2,'captcha_site_key','Captcha Site Key',NULL,'6LcrnOEUAAAAAMqhYt3AY5lXkkNDEWcGfyM8DvP9','Y','text',NULL),(2,'company_title','Company Title','company','DbE Acoustics','Y','text',NULL),(2,'contact_page','Contact Page','plugin','13','Y','embed','Plugin'),(2,'email','Email','contact','info@dbe-id.com','Y','text',NULL),(2,'facebook','Facebook Link','social_media','https://www.facebook.com/DbE-Indonesia-111625933761626/','Y','text',NULL),(2,'featured_category_product','Featured Category Product','plugin','15','Y','embed','Plugin'),(2,'featured_top_image_carousel','Featured Homepage Top Image Carousel',NULL,'2','Y','embed','Slider Banner'),(2,'filename_product_template','Filename Product Template List','template','[\"template_1\",\"template_2\",\"template_3\"]','Y','text','MultiSelect'),(2,'filename_product_template_note','Filename Product Template Note','template','template_1(GM500), template_2(Dbe Comfit Eartips), template_3(dbe hardcase)','Y','text',NULL),(2,'images_upload_note','Images Upload Note',NULL,'Sort Image: SRP Image, Image Detail Product from Top to Down','Y','text',NULL),(2,'image_logo','Image Logo','image','http://localhost/cms-admin/img/Images/20200404/397dc070d9767a4763b013f678d6cb83.png','Y','image',NULL),(2,'instagram','Instagram Link','social_media','https://www.instagram.com/dbe.id/','Y','text',NULL),(2,'menu_footer','Menu Footer',NULL,'1','Y','embed','Page'),(2,'menu_header','Menu Header',NULL,'1','Y','embed','Page'),(2,'our_story_page','Our Story Page','plugin','6','Y','embed','Plugin'),(2,'phone_number','Phone Number','contact','+62 22 423 4642','Y','text',NULL),(2,'product_category_page','Product Category Page Plugin','plugin','4','Y','embed','Plugin'),(2,'section_1','Homepage Section 1','plugin','8','Y','embed','Plugin'),(2,'section_2','Homepage Section 2','plugin','9','Y','embed','Plugin'),(2,'section_3','Homepage Section 3','plugin','10','Y','embed','Plugin'),(2,'section_4','Homepage Section 4','plugin','11','Y','embed','Plugin'),(2,'service_centre','Service Centre List','plugin','12','Y','embed','Plugin'),(2,'shopee','Shopee Link','social_media','https://shopee.co.id/dbeofficial','Y','text',NULL),(2,'tokopedia','Tokopedia Link','social_media','https://www.tokopedia.com/dbeofficial','Y','text',NULL),(2,'top_image_carousel','Homepage Top Image Carousel',NULL,'1','Y','embed','Slider Banner'),(2,'warranty_information_page','Warranty Information Page','plugin','14','Y','embed','Plugin'),(2,'whatsapp_number','WhatsApp Number','contact','6281319991783','Y','text',NULL),(2,'where_to_buy_page','Where to Buy Page','plugin','5','Y','embed','Plugin'),(2,'youtube_review','Youtube Review Plugin','plugin','7','Y','embed','Plugin');
 /*!40000 ALTER TABLE `themes_setting` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -409,4 +451,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-31 23:30:10
+-- Dump completed on 2020-04-08 11:29:27
